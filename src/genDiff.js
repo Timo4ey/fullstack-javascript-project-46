94e384bd800be334da03536adf17a758bfd7034a
Timofey <tima_yakovishin@mail.ru>
2023-07-11 21:31:31 +0300

    [fix] upd9 enrty point

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,13 +1,13 @@
-import { program } from 'commander';
+#!/usr/bin/env node
+
+import { Command } from 'commander';
 import { formatHandler } from './parsers/formatHandler.js';
 // const join = (first, second, connector = '') => `${first}${connector}${second}`;
-
+const program = new Command();
 program
   .name('gendiff')
   .description(' Compares two configuration files and shows a difference.')
-  .version('1.0.0');
-
-program
+  .version('1.0.0')
   .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
@@ -16,10 +16,7 @@ program
     // eslint-disable-next-line no-console
     console.log(formatHandler(first, second, option));
   });
-// .command('join')
-// .description('Команда соединяет две строки в одну')
 
-// BEGIN (write your solution here)
-// END
+program.parse();
 
 export default program;
