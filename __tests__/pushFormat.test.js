a0c60b4c4de341547b93e0dcf0dc0b21297e0831
Timofey <tima_yakovishin@mail.ru>
2023-07-12 16:07:36 +0300

    [refactor] reduce quantity of code.

--- __tests__/pushFormat.test.js
+++ __tests__/pushFormat.test.js
@@ -3,8 +3,6 @@ import { pushFormat } from '../src/formatters/pushFormat.js';
 test('pushFormat Positive tests.', () => {
   expect(pushFormat('item', null, 2, ' ', 'value')[0]).toEqual('    item: value\n');
   expect(pushFormat('item', null, 2, ' ', 'value', 'value')[0]).toEqual('    item: value\n');
-  expect(pushFormat('item', null, 2, '+-', 'value', 'not value')).toEqual([
-    '  + item: value\n',
-    '  - item: not value\n',
-  ]);
+  const res = pushFormat('item', null, 2, '+-', 'value', 'not value');
+  expect(res).toEqual(['  + item: value\n', '  - item: not value\n']);
 });
